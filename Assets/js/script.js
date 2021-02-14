b37b1e6ea3402986079112128d61559fcd051ee3
Dylan Geswelli <dylan.geswelli@gmail.com>
2021-02-13 21:46:41 -0500

    application can now recall planner schedule from each hour, also added more comments to explain functionality of script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,15 +10,33 @@ $("#currentDay").append(currentDay);
 
 
 // Below saves button clicks in each row to local storage //
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(".saveBtn").on("click", function() {
+    $(".saveBtn").on("click", function () {
 
+        // set variables for value and time to be saved in local storage //
+        var value = $(this).siblings(".task").val();
+        var time = $(this).parent().attr("id")
 
-var value = $(this).siblings(".task").val();
-var time = $(this).parent().attr("id")
+        localStorage.setItem(time, value)
+    })
 
-localStorage.setItem(time, value)
 })
 
-})
+
+
+// Need code here to allow the application to check current time and recolor each row accordingly //
+
+
+// below recalls what was stored in the textarea field for each hour from local storage //
+// May be a way to use const and require less lines of code //
+
+$("#hour-9 .task").val(localStorage.getItem("hour-9"));
+$("#hour-10 .task").val(localStorage.getItem("hour-10"));
+$("#hour-11 .task").val(localStorage.getItem("hour-11"));
+$("#hour-12 .task").val(localStorage.getItem("hour-12"));
+$("#hour-13 .task").val(localStorage.getItem("hour-13"));
+$("#hour-14 .task").val(localStorage.getItem("hour-14"));
+$("#hour-15 .task").val(localStorage.getItem("hour-15"));
+$("#hour-16 .task").val(localStorage.getItem("hour-16"));
+$("#hour-17 .task").val(localStorage.getItem("hour-17"));
